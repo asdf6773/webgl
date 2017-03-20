55a1ec149e82b2936aa1c9e3437243194b390d66
asdf6773 <457776773@qq.com>
2017-03-20 17:52:37 +0800

    add color

--- mySketch.js
+++ mySketch.js
@@ -5,8 +5,10 @@ var VSHADER_SOURCE =
     ' gl_PointSize = 10.;' +
     '}';
 var FSHADER_SOURCE =
+    'precision mediump float;' +
+    'uniform vec4 u_FragColor;' +
     'void main(){' +
-    '  gl_FragColor = vec4(0.,0.8 ,0.3,1.);' +
+    '  gl_FragColor = u_FragColor;' +
     '}';
 
 var x;
@@ -16,9 +18,7 @@ var frame = 0;
 function main() {
     var canvas = document.getElementById('webgl');
     var gl = getWebGLContext(canvas);
-    canvas.onmousedown = function(ev) {
-        click(ev, gl, canvas, a_Position);
-    }
+
     if (!gl) {
         console.log('Failed to get the rendering for WebGL');
         return;
@@ -35,29 +35,55 @@ function main() {
         console.log('Failed to get the storage location of a_Position');
         return;
     }
-    gl.vertexAttrib3f(a_Position, 0., 0., 0.);
-    gl.clearColor(0.0, 0.2, 0.3, 1.0);
+
+    canvas.onmousedown = function(ev) {
+        click(ev, gl, canvas, a_Position,u_FragColor);
+    }
+    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (u_FragColor < 0) {
+        console.log('Failed to get the storage location of u_FragColor');
+        return;
+    }
+    //  gl.vertexAttrib3f(a_Position, 0., 0., 0.);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, 1);
+    //  gl.drawArrays(gl.POINTS, 0, 1);
     //  frame += 0.01;
     //  requestAnimationFrame(main);
 }
 var g_points = [];
+var g_colors = [];
 
-function click(ev, gl, canvas, a_Position) {
+function click(ev, gl, canvas, a_Position, u_FragColor) {
     var x = ev.clientX;
     var y = ev.clientY;
     var rect = ev.target.getBoundingClientRect(); //get canvas position
     x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
     y = (-(y - rect.top) + canvas.height / 2) / (canvas.height / 2);
-    g_points.push(x);
-    g_points.push(y);
+    g_points.push([x, y]);
 
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    var len = g_points.length;
-    for (var i = 0; i < len; i += 2) {
-        gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+    if (x >= 0.0 && y >= 0.0) {
+        g_colors.push([1.0, 0.0, 0.0, 1.0]);
+    } else if (x >= 0 && y < 0) {
+        g_colors.push([1.0, 1.0, 0.0, 1.0])
+    } else if (x < 0 && y < 0) {
+        g_colors.push([1.0, 1.0, 1.0, 1.0])
+    } else if (x < 0 && y >= 0) {
+        g_colors.push([1.0, 0.0, 1.0, 1.0])
     }
-    gl.drawArrays(gl.POINTS, 0 , 1);
+
+    gl.clear(gl.COLOR_BUFFER_BIT); // IMPORTANT
+    var len = g_points.length;
+
+    for (var i = 0; i < len; i++) {
+        var xy = g_points[i];
+        var rgba = g_colors[i]
+        gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
+
+
+    console.log(len);
 }
-requestAnimationFrame(main);
+//requestAnimationFrame(main);
