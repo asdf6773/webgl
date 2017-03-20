44dbbb08ab36b2d80cde0a15d6d7e249453c5413
asdf6773 <457776773@qq.com>
2017-03-20 13:50:43 +0800

    add a point

--- mySketch.js
+++ mySketch.js
@@ -1,3 +1,14 @@
+var VSHADER_SOURCE =
+    'void main(){' +
+    ' gl_Position = vec4(0.,0.,0.,1.);' +
+    ' gl_PointSize = 10.;' +
+    '}';
+var FSHADER_SOURCE =
+    'void main(){' +
+    '  gl_FragColor = vec4(1.,0.8 ,0.3,1.);' +
+    '}';
+
+
 function main() {
     var canvas = document.getElementById('webgl');
     var gl = getWebGLContext(canvas);
@@ -5,8 +16,11 @@ function main() {
         console.log('Failed to get the rendering for WebGL');
         return;
     }
-
-gl.clearColor(0.0,1.0,0.0,1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders');
+        return;
+    }
+    gl.clearColor(0.0, 1.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, 1);
 }
