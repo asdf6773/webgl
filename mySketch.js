8629b5b040d7ec0fdb7448d17f8377c7095ff944
asdf6773 <457776773@qq.com>
2017-03-22 00:35:05 +0800

    add rotating animation

--- mySketch.js
+++ mySketch.js
@@ -1,16 +1,8 @@
 var VSHADER_SOURCE =
-    //'uniform vec4 u_Translation;' +
-    'uniform vec2 u_CosBSinB;' +
-    'uniform mat4 u_xformMatrix;' +
+    'uniform mat4 u_ModelMatrix;' +
     'attribute vec4 a_Position;' +
     'void main(){' +
-    'gl_Position = u_xformMatrix*a_Position;' +
-    //  'gl.Position = vec4(u_CosBSinB);'
-    //  ' gl_Position.x = a_Position.x*u_CosBSinB.x - a_Position.y*u_CosBSinB.y;' +
-    //' gl_Position.y = a_Position.x*u_CosBSinB.y + a_Position.y*u_CosBSinB.x;' +
-    //' gl_Position.z = a_Position.z;' +
-    //' gl_Position.w = 1.0;' +
-    //' gl_PointSize = 10.;' +
+    'gl_Position = u_ModelMatrix*a_Position;' +
     '}';
 var FSHADER_SOURCE =
     'precision mediump float;' +
@@ -24,15 +16,16 @@ var frame = 0;
 var Tx = 0.5,
     Ty = 0.5,
     Tz = 0.0;
-var Angle = 90;
+var Angle = 60;
 var radian = Math.PI * Angle / 180;
 var cosB = Math.cos(radian);
 var sinB = Math.sin(radian);
-var xformMatrix = new Float32Array([
-    cosB, sinB, 0.0, 0.0, -sinB, cosB, 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0
-]);
+
+
+  var modelMatrix = new Matrix4();
+
+
+ANGLE_STEP = 45;
 
 function main() {
     var canvas = document.getElementById('webgl');
@@ -47,19 +40,51 @@ function main() {
         return;
     }
     var n = initVertexBuffers(gl);
-    var u_CosBSinB = gl.getUniformLocation(gl.program, 'u_CosBSinB');
-    var u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix')
-    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix);
-    gl.uniform2f(u_CosBSinB, cosB, sinB);
-    //  gl.uniform1f(u_SinB, sinB);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    var currentAngle = 0.0;
+    //var ModelMatrix
 
+    ////////////////////////////////
+    var tick = function() {
+        currentAngle = animate(currentAngle);
+        //    console.log(currentAngle);
+        draw(gl,n, currentAngle, modelMatrix, u_ModelMatrix);
+        requestAnimationFrame(tick);
+    }
+    tick();
+    /////////////////////////////////
+    var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix')
+    console.log(u_ModelMatrix);
+    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
     if (n < 0) {
         console.log('Failed to set the position of the vertices');
     }
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
 }
+
+function draw(gl,n, currentAngle, modelMatrix, u_ModelMatrix) {//LOSING PARAMETER
+    modelMatrix.setRotate(currentAngle, 0, 0, 1);
+    modelMatrix.translate(0, 0.4, 0);
+    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+    gl.clear(gl.DEPTH_BUFFER_BIT);//FORGET SET PARAMETER
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+var g_last = Date.now();
+
+function animate(angle) {
+    var now = Date.now();
+    var elapsed = now - g_last;
+    g_last = now
+    var newAngle = angle + (ANGLE_STEP * elapsed) / 1000.0;
+    return newAngle %= 360;
+}
+
+
+
+
+
+
 
 function initVertexBuffers(gl) {
     var vertices = new Float32Array([-0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5]);
