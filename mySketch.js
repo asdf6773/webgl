5d9e031623edcca9d8c784d680638e163fe3309e
asdf6773 <457776773@qq.com>
2017-03-21 20:28:20 +0800

    transform using matrix

--- mySketch.js
+++ mySketch.js
@@ -1,13 +1,15 @@
 var VSHADER_SOURCE =
     //'uniform vec4 u_Translation;' +
     'uniform vec2 u_CosBSinB;' +
+    'uniform mat4 u_xformMatrix;' +
     'attribute vec4 a_Position;' +
     'void main(){' +
-  //  'gl.Position = vec4(u_CosBSinB);'
-    ' gl_Position.x = a_Position.x*u_CosBSinB.x - a_Position.y*u_CosBSinB.y;' +
-    ' gl_Position.y = a_Position.x*u_CosBSinB.y + a_Position.y*u_CosBSinB.x;' +
-    ' gl_Position.z = a_Position.z;' +
-    ' gl_Position.w = 1.0;' +
+    'gl_Position = u_xformMatrix*a_Position;' +
+    //  'gl.Position = vec4(u_CosBSinB);'
+    //  ' gl_Position.x = a_Position.x*u_CosBSinB.x - a_Position.y*u_CosBSinB.y;' +
+    //' gl_Position.y = a_Position.x*u_CosBSinB.y + a_Position.y*u_CosBSinB.x;' +
+    //' gl_Position.z = a_Position.z;' +
+    //' gl_Position.w = 1.0;' +
     //' gl_PointSize = 10.;' +
     '}';
 var FSHADER_SOURCE =
@@ -23,13 +25,19 @@ var Tx = 0.5,
     Ty = 0.5,
     Tz = 0.0;
 var Angle = 90;
+var radian = Math.PI * Angle / 180;
+var cosB = Math.cos(radian);
+var sinB = Math.sin(radian);
+var xformMatrix = new Float32Array([
+    cosB, sinB, 0.0, 0.0, -sinB, cosB, 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0, 1.0
+]);
 
 function main() {
     var canvas = document.getElementById('webgl');
     var gl = getWebGLContext(canvas);
-    var radian = Math.PI * Angle / 180;
-    var cosB = Math.cos(radian);
-    var sinB = Math.sin(radian);
+
     if (!gl) {
         console.log('Failed to get the rendering for WebGL');
         return;
@@ -40,7 +48,8 @@ function main() {
     }
     var n = initVertexBuffers(gl);
     var u_CosBSinB = gl.getUniformLocation(gl.program, 'u_CosBSinB');
-
+    var u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix')
+    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix);
     gl.uniform2f(u_CosBSinB, cosB, sinB);
     //  gl.uniform1f(u_SinB, sinB);
 
