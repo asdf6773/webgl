2807e97f559bc4df2424abe7942deb7637562a61
asdf6773 <457776773@qq.com>
2017-03-22 23:41:31 +0800

    add color to vertex (stride,BYTES_PER_ELEMENT)

--- mySketch.js
+++ mySketch.js
@@ -1,14 +1,18 @@
 var VSHADER_SOURCE =
     'uniform mat4 u_ModelMatrix;' +
+    'attribute vec4 a_Color;' +
+    'varying vec4 v_Color;' +
     'attribute vec4 a_Position;' +
     'void main(){' +
+    'v_Color = a_Color;' +
     'gl_Position = u_ModelMatrix*a_Position;' +
     '}';
 var FSHADER_SOURCE =
     'precision mediump float;' +
+    'varying vec4 v_Color;' +
     'uniform vec4 u_FragColor;' +
     'void main(){' +
-    '  gl_FragColor = vec4(0.5,0.4,0.4,1.0);' +
+    '  gl_FragColor = v_Color;' +
     '}';
 var x;
 var y;
@@ -22,7 +26,7 @@ var cosB = Math.cos(radian);
 var sinB = Math.sin(radian);
 
 
-  var modelMatrix = new Matrix4();
+var modelMatrix = new Matrix4();
 
 
 ANGLE_STEP = 45;
@@ -48,7 +52,7 @@ function main() {
     var tick = function() {
         currentAngle = animate(currentAngle);
         //    console.log(currentAngle);
-        draw(gl,n, currentAngle, modelMatrix, u_ModelMatrix);
+        draw(gl, n, currentAngle, modelMatrix, u_ModelMatrix);
         requestAnimationFrame(tick);
     }
     tick();
@@ -60,15 +64,15 @@ function main() {
         console.log('Failed to set the position of the vertices');
     }
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
+    gl.drawArrays(gl.POINTS, 0, n);
 }
 
-function draw(gl,n, currentAngle, modelMatrix, u_ModelMatrix) {//LOSING PARAMETER
+function draw(gl, n, currentAngle, modelMatrix, u_ModelMatrix) { //LOSING PARAMETER
     modelMatrix.setRotate(currentAngle, 0, 0, 1);
     modelMatrix.translate(0, 0.4, 0);
     gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
-    gl.clear(gl.DEPTH_BUFFER_BIT);//FORGET SET PARAMETER
-    gl.drawArrays(gl.TRIANGLES, 0, n);
+    gl.clear(gl.DEPTH_BUFFER_BIT); //FORGET SET PARAMETER
+    gl.drawArrays(gl.LINE_LOOP, 0, n);
 }
 var g_last = Date.now();
 
@@ -82,23 +86,27 @@ function animate(angle) {
 
 
 
-
-
-
-
 function initVertexBuffers(gl) {
-    var vertices = new Float32Array([-0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5]);
-    var n = 4;
-    var vertexBuffer = gl.createBuffer();
+    var vertices = new Float32Array([-0.5, 0.5, 1.0, 0.0, 0.0, -0.5, -0.5, 0.0, 1.0, 0.0,
+        0.5, 0.5, 0.0, 0.0, 0.1,
+    ]);
+    var n = 3;
+    var vertexBuffer = gl.createBuffer(); //////////////create buffer
+    //var colorBuffer = gl.createBuffer();
     if (!vertexBuffer) {
         console.log('Failed to create buffer object');
         return -1;
     }
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    var FSIZE = vertices.BYTES_PER_ELEMENT;
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); /////////////bind buffer
+    //  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW); //put data
+    var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, FSIZE * 0);
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
     gl.enableVertexAttribArray(a_Position);
+    gl.enableVertexAttribArray(a_Color);
     return n;
 };
 //requestAnimationFrame(main);
